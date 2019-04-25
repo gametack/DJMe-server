ae7341a571d4c1fd6b358121753f1b618d680672
George Keenan <georgekeenan@gmail.com>
2019-04-24 22:37:39 -0400

    After initial table+API gen

--- App.js
+++ App.js
@@ -1,7 +1,12 @@
+import Amplify from "aws-amplify";
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { withAuthenticator } from 'aws-amplify-react-native';
+import awsConfig from "./aws-exports";
 
-export default class App extends React.Component {
+Amplify.configure(awsConfig);
+
+class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
@@ -19,3 +24,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+export default withAuthenticator(App);
