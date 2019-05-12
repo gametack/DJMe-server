4f85872bdc27838de830e05ff73b543b6575681f
Mark Foresta <Mark.Foresta691@gmail.com>
2019-05-12 18:46:40 -0400

    Change App.js to hit basic create join room page

--- App.js
+++ App.js
@@ -1,28 +1,43 @@
 import Amplify from "aws-amplify";
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import { withAuthenticator } from 'aws-amplify-react-native';
 import awsConfig from "./aws-exports";
+import { ThemeProvider, Button, Text } from 'react-native-elements';
 
 Amplify.configure(awsConfig);
+
+const theme = {
+  colors: {
+    primary: '#92CBC5',
+    secondary: '#000000'
+  },
+  Button: {
+    raised: true,
+    titleStyle: {
+      color:'#000000',
+      fontSize: 30
+    },
+    containerStyle: {
+      width: '50%',
+      marginTop: 20
+    }
+  },
+};
 
 class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+      //TODO: Replace Text with image for logo
+      <View style={{position: 'absolute', top: 0, left: 0, right: 0, bottom: 0, justifyContent: 'center', alignItems: 'center', backgroundColor:'#303030'}}>
+      <ThemeProvider theme={theme}>
+        <Text h1Style={{color: 'red', fontSize: 100}} h1>DJME</Text>
+        <Button containerStyle={{marginTop: 100}} title="JOIN" />
+        <Button containerStyle={{width: '35%'}}title="CREATE" />
+      </ThemeProvider>
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
 
 export default withAuthenticator(App);
